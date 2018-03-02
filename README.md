3a9ba70349ff16b726fe6bed23d1b1487db92fdc
root <root@oselab-86ca.oslab.opentlc.com>
2018-03-02 13:12:51 +0100

    adding dns, and some postdeploy things

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # homework
+This is a quick and dirty way to automate the homework i needed to do for "Advanced OpenShift Deployment" workshop
+This is very strict focusing on the "Homework" environment which i can order on the enablement Hub  / OpenTLC.
+
+I'm using oslab.opentlc.com as my jump host.
+
+1) Doing some stuff to get ansible up and running on "oslab.opentlc.com"
+.. not automated yet
+
+2) Doing some prep on oslab to get ansible up and working
+register
+subscription
+install ansible
+
+3) Getting DNS set up and ready
+There is an installer to get the wildcard records set up somehow:
+http://www.opentlc.com/download/ose_advanced/resources/3.1/oselab.ha.dns.installer.sh
+I'Ve used that and added a second script to run my own example.com domain (as the one provides has issues, people don't fix).
+
+dns_installer.sh <- installs dns, configures dns to serve wildcard-record
+              please adjust "domain" variable to suit your needs
+create_local_dns.sh <- expands the dns server to also server for example.com and it's reverse domain
+
+
+NOW Ansible is working
+you will always match to the "hosts" file provided in this repository
+so:
+ansible -i hosts all .....
+or 
+ansible-playbook -i hosts ....yml
+
+4) get subscriptions on all machines 
+to be automated yet
+
+5) some additional prep
+  - timezone
+  - dns resolver adjustment (not there yet)
+pre_deploy.yml  - predeployment
+
+
+6) run OCP installation
+
+
+cicd.yml
+create_pv_definition.sh
+hosts
+htpasswd.openshift
+post_deploy.yml
---
+++ create_local_dns.sh
@@ -0,0 +1,95 @@
+#/bin/bash
+
+DOM=example.com
+REV=0.168.192.in-addr.arpa
+NAMESERVER=oselab.example.com  
+ocp_hosts_file=$HOME/projects/ocp_homework/hosts 
+zone_file=/var/named/zones/${DOM}.db
+reverse_zone_file=/var/named/zones/${REV}.db
+named_conf=/etc/named.conf
+tmp_file=$(mktemp)
+
+export DOM NAMESERVER ocp_hosts_file
+
+function forw_record {
+     echo -n "$1. IN A "
+     dig +short $1
+}
+
+function rev_record {
+     dig +short $1 | sed -e 's:\([0-9]*\)\.\([0-9]*\)\.\([0-9]*\)\.\([0-9]*\):\4.\3.\2.\1.in-addr.arpa. IN PTR '$1'.:'
+}
+
+function zone_head {
+cat << EOF
+\$ORIGIN  .
+\$TTL 1  ;  1 seconds (for testing only)
+${1}. IN SOA ${NAMESERVER}.  root.${NAMESERVER}.  (
+  2011112904  ;  serial
+  60  ;  refresh (1 minute)
+  15  ;  retry (15 seconds)
+  1800  ;  expire (30 minutes)
+  10  ; minimum (10 seconds)
+)
+  NS ${NAMESERVER}.
+\$ORIGIN ${1}.
+
+EOF
+}
+
+function add_zone_to_named_conf  {
+cat << EOF
+
+zone "$1" IN {
+  type master;
+  file "zones/$1.db";
+  allow-update { key cloudapps-86ca.oslab.opentlc.com ; } ;
+};
+EOF
+}
+
+function add_forwarders_to_named_conf {
+   gawk '
+      /options / { 
+           print ; 
+           print "        forwarders {"
+           print "              8.8.8.8 ;"
+           print "              8.8.4.4 ;"
+           print "              } ;"
+           next ;
+        }
+        { print }
+      
+' $named_conf 
+}
+
+
+#### MAIN
+## create forward zone
+zone_head $DOM > $zone_file
+forw_record $NAMESERVER >> $zone_file
+awk '/'$DOM'/ { print $1 }' $ocp_hosts_file | sed -e 's/#//g' | while read name
+do
+ forw_record $name >> $zone_file
+done
+
+## create reverse zone
+zone_head $REV > $reverse_zone_file
+rev_record $NAMESERVER >> $reverse_zone_file
+awk '/'$DOM'/ { print $1 }' $ocp_hosts_file | sed -e 's/#//g' | while read name
+do
+ rev_record $name >> $reverse_zone_file
+done
+
+
+## add zone to nameserver
+
+grep -q "$DOM" $named_conf || add_zone_to_named_conf $DOM >> $named_conf
+grep -q "$REV" $named_conf || add_zone_to_named_conf $REV >> $named_conf
+grep -q "forwarders" $named_conf || { 
+    add_forwarders_to_named_conf > $tmp_file
+    mv $tmp_file $named_conf
+    chmod 640 $named_conf
+    chgrp named $named_conf
+}
+
